7754c671eebab041e7a0c31c48cefd99d8a0aad6
dirmeier <simon.dirmeier@bsse.ethz.ch>
2018-10-22 21:02:20 +0200

    Removes jave

--- old/src/main/java/net/digital_alexandria/r__/parser/AbstractSyntaxSubtree.java
+++
@@ -1,9 +0,0 @@
-package net.digital_alexandria.r__.parser;
-
-/**
- * @author Simon Dirmeier {@literal mail@simon-dirmeier.net}
- */
-public abstract class AbstractSyntaxSubtree
-{
-    public abstract Object traverse();
-}
